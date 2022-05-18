97427d147f694212219ed816bba433af9338425d
joshua-horne <joshua.lee.horne@gmail.com>
2022-05-18 17:07:55 +1200

    basic styling to NewGame

--- src/app/components/NewGame.jsx
+++ src/app/components/NewGame.jsx
@@ -1,38 +1,60 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
-import { firstCard, newPlayer } from '../actions';
+import {
+  Button,
+  Image,
+  Input,
+  InputGroup,
+  InputRightAddon,
+  List,
+  ListItem,
+  Stack,
+} from '@chakra-ui/react'
+import { PlusSquareIcon } from '@chakra-ui/icons'
+import cupImg from '../../lib/images/cup.png'
+
+import { firstCard, newPlayer } from '../actions'
 
 function NewGame() {
-  let [playerName, setName] = useState('');
-  const running = useSelector((state) => state.running);
-  const playerList = useSelector((state) => state.players);
+  let [playerName, setName] = useState('')
+  const running = useSelector((state) => state.running)
+  const playerList = useSelector((state) => state.players)
 
   function handleSubmit() {
-    dispatch(newPlayer(playerName));
-    setName('');
+    dispatch(newPlayer(playerName))
+    setName('')
   }
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   return !running ? (
-    <div>
-      <label htmlFor="playerName">Enter Player Names: </label>
-      <input
-        type="text"
-        id="palyerName"
-        value={playerName}
-        onChange={(evt) => setName(evt.target.value)}
-      />
-      <button onClick={handleSubmit}>Submit</button>
-      <ul>
+    <Stack spacing={3}>
+      <Image src={cupImg}></Image>
+      <List spacing={3}>
         {playerList.map((player, i) => {
-          return <li key={i}>{player}</li>;
+          return <ListItem key={i}>{player}</ListItem>
         })}
-      </ul>
-      <button onClick={() => dispatch(firstCard())}>New Game</button>
-    </div>
-  ) : null;
+      </List>
+      <InputGroup>
+        <Input
+          type="text"
+          id="palyerName"
+          placeholder="Add a player"
+          value={playerName}
+          onChange={(evt) => setName(evt.target.value)}
+        />
+        <InputRightAddon>
+          <PlusSquareIcon m={3} onClick={handleSubmit}>
+            +
+          </PlusSquareIcon>
+        </InputRightAddon>
+      </InputGroup>
+      <Button m={6} onClick={() => dispatch(firstCard())}>
+        Start Game
+      </Button>
+    </Stack>
+  ) : null
 }
 
-export default NewGame;
+export default NewGame
