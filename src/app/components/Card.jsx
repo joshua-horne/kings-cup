a5f4d3747a73581e56b2f602e8fa6fe5bdb2cad3
Joshua Horne <joshua.lee.horne@gmail.com>
2022-06-28 22:47:11 +1200

    active plyer and next player names displayed and loop through player list

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -9,19 +9,30 @@ import { fetchEffect, receiveKing } from '../actions'
 import Replay from './Replay'
 
 function Card() {
-  let [cardEffect, setEffect] = useState('')
-  let [activePlayer, setPlayer] = useState(0)
-
   const card = useSelector((state) => state.card)
   const kingCounter = useSelector((state) => state.kingCounter)
   const playerList = useSelector((state) => state.players)
+
+  let [cardEffect, setEffect] = useState('')
+  let [activeIndex, setIndex] = useState(0)
+  let [nextIndex, setNext] = useState(1)
 
   const dispatch = useDispatch()
 
   useEffect(() => {
     setEffect(fetchEffect(card.value))
-    //TODO: setPlayer to +1 of active player BUT needs to loop on playerList.length
-    setPlayer(playerList[0])
+
+    if (nextIndex == playerList.length - 1) {
+      setIndex(activeIndex + 1)
+      setNext(0)
+    } else if (nextIndex == 0) {
+      setIndex(0)
+      setNext(nextIndex + 1)
+    } else {
+      setIndex(activeIndex + 1)
+      setNext(nextIndex + 1)
+    }
+
     if (card.value == 'KING' && kingCounter == 1) {
       setEffect(fetchEffect('FINAL KING'))
       dispatch(receiveKing())
@@ -30,14 +41,14 @@ function Card() {
 
   return (
     <Stack>
-      <Heading>Your Card {activePlayer}!</Heading>
+      <Heading>Your Card {playerList[activeIndex]}!</Heading>
       <Image src={card.image} />
       <Heading>
         Card Effect: {cardEffect} <QuestionIcon />
       </Heading>
       <Text>Remaining Cards: {card.remaining}</Text>
       <Text>Remaining Kings: {kingCounter}</Text>
-      <Heading> Next Turn: {activePlayer} </Heading>
+      <Heading> Next Turn: {playerList[nextIndex]} </Heading>
       <Replay />
     </Stack>
   )
